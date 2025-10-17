459585f95fca1370b2eea6f53cd58b1677fecb05
verbey <bivanestor@tutanota.com>
2025-10-17 16:26:11 +0000

    Persist data stored in the sessionStore Zustand store

--- src/stores/sessionStore.ts
+++ src/stores/sessionStore.ts
@@ -1,27 +1,37 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 import { ICreateClientOpts } from "matrix-js-sdk";
 
 interface SessionStore {
   clientData: ICreateClientOpts[];
-  addClientData: (newClientData: ICreateClientOpts) => void;
   activeClientIndex: number;
-  setActiveClientIndex: (newActiveClientUserId: string) => void;
 }
 
-const useSessionStore = create<SessionStore>()((set) => ({
-  clientData: [],
-  addClientData: (newClientData) =>
-    set((state) => ({
-      clientData: [...state.clientData, newClientData],
-    })),
-  activeClientIndex: 0,
-  setActiveClientIndex: (newActiveClientUserId) => {
-    set((state) => ({
-      activeClientIndex: state.clientData.findIndex(
-        (MatrixClient) => MatrixClient.userId === newActiveClientUserId
-      ),
-    }));
-  },
-}));
+const useSessionStore = create<SessionStore>()(
+  persist(
+    (set) => ({
+      clientData: [],
+      addClientData: (newClientData: ICreateClientOpts) =>
+        set((state) => ({
+          clientData: [...state.clientData, newClientData],
+        })),
+      activeClientIndex: 0,
+      setActiveClientIndex: (newActiveClientUserId: string) => {
+        set((state) => ({
+          activeClientIndex: state.clientData.findIndex(
+            (MatrixClient) => MatrixClient.userId === newActiveClientUserId
+          ),
+        }));
+      },
+    }),
+    {
+      name: "session-store",
+      partialize: (state) => ({
+        clientData: state.clientData,
+        activeClientIndex: state.activeClientIndex,
+      }),
+    }
+  )
+);
 
 export default useSessionStore;
