57346006d2b3122102ea0d5daaa51114c6fc3f73
Hnikar <140448550+Hnikar@users.noreply.github.com>
2025-10-21 19:11:59 +0000

    Refactor RegisterForm to implement conditional rendering of from fields

--- src/features/registration/components/RegistrationForm.tsx
+++ src/features/registration/components/RegistrationForm.tsx
@@ -10,7 +10,7 @@ import Link from "next/link";
 import { useRegistrationForm } from "../hooks/useRegisrationForm";
 
 export function RegisterForm() {
-	const { form, onSubmit } = useRegistrationForm();
+	const { form, onSubmit, availableFlows } = useRegistrationForm();
 
 	return (
 		<div className="w-full max-w-sm">
@@ -30,63 +30,65 @@ export function RegisterForm() {
 								</FormItem>
 							)}
 						/>
+						{availableFlows.includes("m.login.dummy") && (
+							<div className="grid gap-2 mt-2">
+								<FormField
+									control={form.control}
+									name="username"
+									render={({ field }) => (
+										<FormItem className="grid gap-2">
+											<FormLabel htmlFor="username">Username</FormLabel>
+											<FormControl>
+												<Input id="username" placeholder="JohnDoe" {...field} />
+											</FormControl>
+											<FormMessage />
+										</FormItem>
+									)}
+								/>
+								{availableFlows.includes("m.login.email.identity") && (
+									<FormField
+										control={form.control}
+										name="email"
+										render={({ field }) => (
+											<FormItem className="grid gap-2">
+												<FormLabel htmlFor="email">Email</FormLabel>
+												<FormControl>
+													<Input id="email" placeholder="johndoe@mail.com" type="email" autoComplete="email" {...field} />
+												</FormControl>
+												<FormMessage />
+											</FormItem>
+										)}
+									/>
+								)}
+								<FormField
+									control={form.control}
+									name="password"
+									render={({ field }) => (
+										<FormItem className="grid gap-2">
+											<FormLabel htmlFor="password">Password</FormLabel>
+											<FormControl>
+												<PasswordInput id="password" placeholder="******" autoComplete="new-password" {...field} />
+											</FormControl>
+											<FormMessage />
+										</FormItem>
+									)}
+								/>
 
-						<FormField
-							control={form.control}
-							name="username"
-							render={({ field }) => (
-								<FormItem className="grid gap-2">
-									<FormLabel htmlFor="username">Username</FormLabel>
-									<FormControl>
-										<Input id="username" placeholder="JohnDoe" {...field} />
-									</FormControl>
-									<FormMessage />
-								</FormItem>
-							)}
-						/>
-
-						<FormField
-							control={form.control}
-							name="email"
-							render={({ field }) => (
-								<FormItem className="grid gap-2">
-									<FormLabel htmlFor="email">Email</FormLabel>
-									<FormControl>
-										<Input id="email" placeholder="johndoe@mail.com" type="email" autoComplete="email" {...field} />
-									</FormControl>
-									<FormMessage />
-								</FormItem>
-							)}
-						/>
-
-						<FormField
-							control={form.control}
-							name="password"
-							render={({ field }) => (
-								<FormItem className="grid gap-2">
-									<FormLabel htmlFor="password">Password</FormLabel>
-									<FormControl>
-										<PasswordInput id="password" placeholder="******" autoComplete="new-password" {...field} />
-									</FormControl>
-									<FormMessage />
-								</FormItem>
-							)}
-						/>
-
-						<FormField
-							control={form.control}
-							name="confirmPassword"
-							render={({ field }) => (
-								<FormItem className="grid gap-2">
-									<FormLabel htmlFor="confirmPassword">Confirm Password</FormLabel>
-									<FormControl>
-										<PasswordInput id="confirmPassword" placeholder="******" autoComplete="new-password" {...field} />
-									</FormControl>
-									<FormMessage />
-								</FormItem>
-							)}
-						/>
-
+								<FormField
+									control={form.control}
+									name="confirmPassword"
+									render={({ field }) => (
+										<FormItem className="grid gap-2">
+											<FormLabel htmlFor="confirmPassword">Confirm Password</FormLabel>
+											<FormControl>
+												<PasswordInput id="confirmPassword" placeholder="******" autoComplete="new-password" {...field} />
+											</FormControl>
+											<FormMessage />
+										</FormItem>
+									)}
+								/>
+							</div>
+						)}
 						<Button type="submit" className="w-full">
 							Register
 						</Button>
